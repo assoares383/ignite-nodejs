16013d32a3c42946d60d54a52198a33cad3a3475
= <alexandressoares383@gmail.com>
2025-05-28 18:44:01 -0300

    test: add teste e2e do registro

--- 03-api-solid/src/http/controllers/register.controller.spec.ts
+++ 03-api-solid/src/http/controllers/register.controller.spec.ts
@@ -1,5 +1,25 @@
-import { expect, test } from "vitest";
+import { app } from '@/app';
+import { after } from 'node:test';
+import request from 'supertest';
+import { beforeAll, describe, expect, test } from "vitest";
 
-test('testing', () => {
-  expect(1 + 1).toEqual(2)
+describe('Register Controller (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  })
+
+  after(async () => {
+    await app.close();
+  })
+  test('should be able to register', async () => {
+    const response = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456'
+      })
+
+    expect(response.statusCode).toEqual(201);
+  })
 })
