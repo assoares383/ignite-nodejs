4c38ea81f76e5d33195072335882dee7ab282e6b
= <alexandressoares383@gmail.com>
2025-05-29 16:41:03 -0300

    feat: refresh token

--- 03-api-solid/src/http/controllers/users/routes.ts
+++ 03-api-solid/src/http/controllers/users/routes.ts
@@ -1,5 +1,6 @@
 import { authenticate } from "@/http/controllers/users/authenticate.controller";
 import { profile } from "@/http/controllers/users/profile.controller";
+import { refresh } from "@/http/controllers/users/refresh.controller";
 import { register } from "@/http/controllers/users/register.controller";
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
 import { FastifyInstance } from "fastify";
@@ -7,6 +8,8 @@ import { FastifyInstance } from "fastify";
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   // Authenticated routes
   app.get('/me', { onRequest: [verifyJWT] }, profile)
