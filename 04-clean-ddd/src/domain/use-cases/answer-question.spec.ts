09404cb176734815af6691c986825654de72a9b7
= <alexandressoares383@gmail.com>
2025-05-31 18:53:26 -0300

    feat: add dependencia externa

--- 04-clean-ddd/src/domain/use-cases/answer-question.spec.ts
+++ 04-clean-ddd/src/domain/use-cases/answer-question.spec.ts
@@ -1,9 +1,18 @@
 import { expect, test } from 'vitest'
+import { Answer } from '../entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
-test('create an answer', () => {
-  const answerQuestion = new AnswerQuestionUseCase()
 
-  const answer = answerQuestion.execute({
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return;
+  }
+}
+
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+  const answer = await answerQuestion.execute({
     questionId: '1',
     instructorId: '2',
     content: 'Nova resposta'
