945651fa0a92daddfebf91d8af145df328a606e7
= <alexandressoares383@gmail.com>
2025-06-05 12:47:01 -0300

    feat: caso de uso - criar pergunta

---
+++ 04-clean-ddd/src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,20 @@
+import { Question } from '../../enterprise/entities/question'
+import { QuestionsRepository } from '../repositories/question-repository'
+import { CreateQuestionUseCase } from './create-question'
+
+const fakeQuestionsRepository: QuestionsRepository = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  create: async (question: Question) => {},
+}
+
+test('create an answer', async () => {
+  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository)
+
+  const { question } = await createQuestion.execute({
+    authorId: '1',
+    title: 'Nova pergunta',
+    content: 'Conteudo da pergunta',
+  })
+
+  expect(question.id).toBeTruthy()
+})
