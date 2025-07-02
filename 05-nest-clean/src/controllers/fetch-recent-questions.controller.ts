c90d2d0a934168d1c552f229fe54413af726e1e7
= <alexandressoares383@gmail.com>
2025-07-01 22:57:56 -0300

    feat: controller de listagem de pergunta

---
+++ 05-nest-clean/src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Query, UseGuards } from "@nestjs/common";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { z } from "zod";
+import { JwtAuthGuard } from "../auth/jwt-auth.guard";
+import { PrismaService } from "../prisma/prisma.service";
+
+const pageQueryParamsSchema = z
+  .string()
+  .optional()
+  .default("1")
+  .transform(Number)
+  .pipe(z.number().min(1));
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamsSchema);
+
+type PageQueryParamsSchema = z.infer<typeof pageQueryParamsSchema>;
+
+@Controller("/questions")
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionsController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get()
+  async handle(
+    @Query("page", queryValidationPipe) page: PageQueryParamsSchema
+  ) {
+    const perPage = 1;
+    const questions = await this.prisma.question.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return {
+      questions,
+    };
+  }
+}
