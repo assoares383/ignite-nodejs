0df359ac80bc8391e32b55fbbc38543b46e080ff
= <alexandressoares383@gmail.com>
2025-07-09 18:11:02 -0300

    refactor: refatorando controller de autenticacao

--- 05-nest-clean/src/infra/http/http.module.ts
+++ 05-nest-clean/src/infra/http/http.module.ts
@@ -1,6 +1,9 @@
+import { AuthenticateStudentUseCase } from "@/domain/forum/application/use-cases/authenticate-student";
 import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
 import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions";
+import { RegisterStudentUseCase } from "@/domain/forum/application/use-cases/register-student";
 import { Module } from "@nestjs/common";
+import { CryptographyModule } from "../cryptography/cryptography.module";
 import { DatabaseModule } from "../database/database.module";
 import { AuthenticateController } from "./controllers/authenticate.controller";
 import { CreateAccountController } from "./controllers/create-account.controller";
@@ -8,7 +11,7 @@ import { CreateQuestionController } from "./controllers/create-question.controll
 import { FetchRecentQuestionsController } from "./controllers/fetch-recent-questions.controller";
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CryptographyModule],
   controllers: [
     AuthenticateController,
     CreateAccountController,
@@ -17,7 +20,9 @@ import { FetchRecentQuestionsController } from "./controllers/fetch-recent-quest
   ],
   providers: [
     CreateQuestionUseCase,
-    FetchRecentQuestionsUseCase
+    FetchRecentQuestionsUseCase,
+    RegisterStudentUseCase,
+    AuthenticateStudentUseCase
   ]
 })
 export class HttpModule { }
