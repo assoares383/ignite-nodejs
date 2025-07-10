1d14fd45396361dc960e04ed2517778d3fba5f35
= <alexandressoares383@gmail.com>
2025-07-10 18:21:25 -0300

    feat: Controller - Buscar pergunta por slug

--- 05-nest-clean/src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ 05-nest-clean/src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -1,5 +1,6 @@
 import { Either, left, right } from '@/core/either'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { Injectable } from '@nestjs/common'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/question-repository'
 
@@ -13,9 +14,9 @@ type GetQuestionBySlugUseCaseResponse = Either<
     question: Question
   }
 >
-
+@Injectable()
 export class GetQuestionBySlugUseCase {
-  constructor(private questionsRepository: QuestionsRepository) {}
+  constructor(private questionsRepository: QuestionsRepository) { }
 
   async execute({
     slug,
