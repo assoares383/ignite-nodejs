d5c458eba8625b6f30331c9b18e04e40cbdf9f1c
= <alexandressoares383@gmail.com>
2025-07-14 20:49:56 -0300

    feat: controller - editar pergunta

--- 05-nest-clean/src/infra/http/http.module.ts
+++ 05-nest-clean/src/infra/http/http.module.ts
@@ -1,5 +1,6 @@
 import { AuthenticateStudentUseCase } from "@/domain/forum/application/use-cases/authenticate-student";
 import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
+import { EditQuestionUseCase } from "@/domain/forum/application/use-cases/edit-question";
 import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions";
 import { GetQuestionBySlugUseCase } from "@/domain/forum/application/use-cases/get-question-by-slug";
 import { RegisterStudentUseCase } from "@/domain/forum/application/use-cases/register-student";
@@ -9,6 +10,7 @@ import { DatabaseModule } from "../database/database.module";
 import { AuthenticateController } from "./controllers/authenticate.controller";
 import { CreateAccountController } from "./controllers/create-account.controller";
 import { CreateQuestionController } from "./controllers/create-question.controller";
+import { EditQuestionController } from "./controllers/edit-question.controller";
 import { FetchRecentQuestionsController } from "./controllers/fetch-recent-questions.controller";
 import { GetQuestionBySlugController } from "./controllers/get-question-by-slug.controller";
 
@@ -19,14 +21,16 @@ import { GetQuestionBySlugController } from "./controllers/get-question-by-slug.
     CreateAccountController,
     CreateQuestionController,
     FetchRecentQuestionsController,
-    GetQuestionBySlugController
+    GetQuestionBySlugController,
+    EditQuestionController
   ],
   providers: [
     CreateQuestionUseCase,
     FetchRecentQuestionsUseCase,
     RegisterStudentUseCase,
     AuthenticateStudentUseCase,
-    GetQuestionBySlugUseCase
+    GetQuestionBySlugUseCase,
+    EditQuestionUseCase
   ]
 })
 export class HttpModule { }
