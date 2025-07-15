dfdba2dd8866eceb090d546cac5488645013a574
= <alexandressoares383@gmail.com>
2025-07-15 18:33:35 -0300

    feat: controller - commentar pergunta

--- 05-nest-clean/src/domain/forum/application/use-cases/comment-on-question.ts
+++ 05-nest-clean/src/domain/forum/application/use-cases/comment-on-question.ts
@@ -1,6 +1,7 @@
 import { Either, left, right } from '@/core/either'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { Injectable } from '@nestjs/common'
 import { QuestionComment } from '../../enterprise/entities/question-comment'
 import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
 import { QuestionsRepository } from '../repositories/question-repository'
@@ -17,12 +18,12 @@ type CommentOnQuestionUseCaseResponse = Either<
     questionComment: QuestionComment
   }
 >
-
+@Injectable()
 export class CommentOnQuestionUseCase {
   constructor(
     private questionsRepository: QuestionsRepository,
     private questionCommentsRepository: QuestionCommentsRepository,
-  ) {}
+  ) { }
 
   async execute({
     authorId,
